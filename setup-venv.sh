09df9321547d6226826ed07a211cb69b512c6df4
Tuomo Björk <tuomo.bjork@tbk.fi>
2024-04-19 14:01:01 +0300

    prepare: setup for flask/frontend

--- setup-venv.sh
+++ setup-venv.sh
@@ -2,8 +2,21 @@
 # This script should be run from inside the project root directory.
 # Else the virtual environment will not be created in the proper location relative to "main.py".
 
-echo "Please wait... Creating virtual environment for Python."
-python -m venv .venv/
+# Test how python likes to be invoked
+if command -v python3 &>/dev/null; then
+    pythonex="python3"
+    echo "Found python: 'python3'"
+    echo "Please wait... Creating virtual environment for Python."
+elif command -v python &>/dev/null; then
+    pythonex="python"
+    echo "Found python: 'python'"
+    echo "Please wait... Creating virtual environment for Python."
+else
+    echo "Error: Couldn't invoke python"
+    exit 1
+fi
+
+$pythonex -m venv .venv/
 
 # Check if user is on Windows, otherwise activate via source.
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
