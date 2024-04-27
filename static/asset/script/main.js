bb7d569f3b75ad91e16756ec9cfb563a628b747e
Niskahiki <99483477+Niskahiki@users.noreply.github.com>
2024-04-27 17:23:12 +0300

    Add screen changing function
    Add function that changes the displayed screen.
    Things to note.
    Screen changing function must be called before interacting with the
    screen otherwise the screen might not work as intended.

--- static/asset/script/main.js
+++ static/asset/script/main.js
@@ -2,8 +2,30 @@
 
 import * as Map from './map.js'
 
-function main() {
-    Map.show();
-}
+/**
+ * Change displayed screen
+ * @param {Number} screenID ID of wanted screen. (0 = '#screen1', 1 = '#screen2', 2 = '#screen3')
+ */
+function changeScreen(screenID) {
+    const activeScreens = document.querySelectorAll('.active-screen');
+    activeScreens.forEach((screen) => {
+        screen.classList.remove('active-screen');
+    });
 
-main();
+    switch (screenID) {
+        case 0:
+            document.querySelector('#screen1').classList.add('active-screen');
+            break;
+
+        case 1:
+            document.querySelector('#screen2').classList.add('active-screen');
+            break;
+
+        case 2:
+            document.querySelector('#screen3').classList.add('active-screen');
+            break;
+
+        default:
+            break;
+    }
+}
