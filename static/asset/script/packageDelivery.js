dd3f90ef570cc314b3678a63ea7f3a57e7f12add
Niskahiki <99483477+Niskahiki@users.noreply.github.com>
2024-05-02 20:57:21 +0300

    Add getPackageLocations function

--- static/asset/script/packageDelivery.js
+++ static/asset/script/packageDelivery.js
@@ -1,6 +1,26 @@
 import * as Map from './map.js'
 
+/**
+ * Get every parcel location from parcels array
+ * @param {Array<Object>} parcels Array of parcel objects 
+ * @returns {Array<Array<Number, Number>>} Array of parcel coords
+ */
+function getPackageLocations(parcels) {
+    let parcelLocations = [];
+    parcels.array.forEach(parcel => {
+        parcelLocations.push(parcel.location);
+    });
+    return parcelLocations;
+}
+
+/**
+ * Start package delivery for player
+ * @param {Object} player Player data object
+ */
 export function start(player) {
     Map.clearMap();
     Map.createPlayerMarker(player.location);
+
+    const packageCoords = getPackageLocations(player.parcels_picked);
+    Map.addPackageDeliveryMarkers(packageCoords);
 } 
